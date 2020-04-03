5771385d82a9c6e4a53024a395590a366e9aa952
unknown <53538316+grlucy@users.noreply.github.com>
2020-04-03 12:22:22 -0400

    set up MVC file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+function App() {
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Hello World</h1>
+        <p className="subtitle">
+          My first website with <strong>Bulma</strong>!
         </p>
       </div>
-    );
-  }
+    </section>
+  );
 }
 
 export default App;
