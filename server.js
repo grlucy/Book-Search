fe501efbca2b630f26c5e96d9a5e56ef2d0e4120
unknown <53538316+grlucy@users.noreply.github.com>
2020-04-04 20:50:40 -0400

    connected mongoose db, added controller and routes

--- server.js
+++ server.js
@@ -3,16 +3,26 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
